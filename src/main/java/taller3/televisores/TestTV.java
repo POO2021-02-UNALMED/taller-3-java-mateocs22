3da25e64bbe0c81f46ce7c5f7fd00b0a92829312
mateocs22 <mcarvajalsa@unal.edu.co>
2021-11-02 18:34:49 -0500

    Update TestTV.java

--- src/main/java/taller3/televisores/TestTV.java
+++ src/main/java/taller3/televisores/TestTV.java
@@ -1,29 +1,30 @@
 package taller3.televisores;
 
-public class TestTV {
+public class TestTv {
 	public static void main(String[] args) {
-		Marca marca1 = new Marca("Semsung");
-	    Marca marca2 = new Marca("Lj");
+		Marca marca =  new Marca("Semsung");
+		TV tv1 =  new TV(marca, true);
 		
-	    TV tv1 = new TV(marca1, true);
-	    TV tv2 = new TV(marca2, false);
+		tv1.setVolumen(5);
+		tv1.volumenUp();
+		
+		TV tv2 =  new TV(marca, false);
+		Control control =  new Control();
+		control.enlazar(tv2);
+		control.turnOn();
+		control.volumenUp();
+		
+		TV tv3 =  new TV(marca, true);
+		tv3.setVolumen(7);
+		tv3.volumenUp();
+		
+		boolean ok = false;
+		
+		if(tv1.getVolumen() == 6 && tv2.getVolumen() == 2 && tv3.getVolumen() == 7) {
+			ok = true;
 	    
-	    tv1.setPrecio(2000);
-	    tv2.setCanal(90);
-	    tv1.setCanal(121);
-		tv2.setVolumen(7);
-		
-		Control control1 = new Control();
-		control1.enlazar(tv1);
-		control1.turnOff();
-		control1.setCanal(50);
-		control1.turnOn();
-		control1.canalUp();
-		control1.volumenUp();
-		
-	    System.out.println(tv2.getCanal());
-	    System.out.println(tv1.getPrecio());
-	    System.out.println(tv1.getMarca().getNombre());
-	    System.out.println(tv1.getCanal());
+		System.out.println(ok);
 	}
+	
+}
 }
